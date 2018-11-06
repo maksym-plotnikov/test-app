5e452ebe0f03c7551a15c2680cd9e92180e16f4a
maksym,plotnikov <maksym.plotnikov@lemberg.co.uk>
2018-11-06 14:20:35 +0200

    change GH url

--- src/app/constants/urls.ts
+++ src/app/constants/urls.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:4200/'; //'https://maksym-plotnikov.github.io/test-app';
+export const API_URL = 'https://maksym-plotnikov.github.io/test-app';
 export const BASE_ROUTE = '/products';
